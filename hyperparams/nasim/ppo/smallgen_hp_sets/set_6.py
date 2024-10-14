c2a1f8c3c04bf0fa1d9a1537220cf47d1c64f483
Raphael Simon <ra.simon1998@gmail.com>
2024-10-14 15:42:00 +0200

    Fixed

--- hyperparams/nasim/ppo/smallgen_hp_sets/set_6.py
+++ hyperparams/nasim/ppo/smallgen_hp_sets/set_6.py
@@ -12,18 +12,18 @@ hyperparams = {
         policy="MlpPolicy",
         batch_size=512,
         n_steps=1024,
-        gamma=0.98,
+        gamma=0.95,
         learning_rate=0.0003,
-        ent_coef=0.001,
+        ent_coef=3e-05,
         clip_range=0.2,
-        n_epochs=5,
-        max_grad_norm=0.8,
-        vf_coef=0.45956389290922794,
+        n_epochs=20,
+        max_grad_norm=2,
+        vf_coef=0.03641294489609048,
         policy_kwargs=dict(
             activation_fn=torch.nn.Tanh,
-            #net_arch=dict(pi=[64], vf=[64]),             # Tiny
+            net_arch=dict(pi=[64], vf=[64]),             # Tiny
             #net_arch=dict(pi=[64, 64], vf=[64, 64]),     # Small
-            net_arch=dict(pi=[128, 128], vf=[128, 128]), # Medium
+            #net_arch=dict(pi=[128, 128], vf=[128, 128]), # Medium
         ),
     )
 }
